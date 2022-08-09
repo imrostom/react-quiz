ba2f9621d3886eeda431fedab675f818e35021dd
Rostom Ali <rostomali4444@gmail.com>
2022-08-09 16:30:21 +0600

    homepage mastering

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import './styles/App.css';
+
+function App() {
+    return (
+        <Layout>
+            <Home></Home>
+        </Layout>
+    );
+}
+
+export default App;
